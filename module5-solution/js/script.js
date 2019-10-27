c0328be329154cf505946596ecde709bab801aaf
Jingting <fengshahuang@gmail.com>
2019-10-26 21:43:30 -0700

    dotted the short name property of object

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -98,12 +98,13 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
       var randomCategory = chooseRandomCategory(categories);
+
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
 
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 'randomCategoryShortName', "'"+randomCategory+"'");
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 'randomCategoryShortName', "'"+randomCategory.short_name+"'");
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
